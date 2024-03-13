083e0bf1603849c17acad7ec7231733f57e025f4
kathir <kathir@centiizen.com>
2024-03-13 12:32:57 +0530

    frontend deploy

--- backend/server.ts
+++ backend/server.ts
@@ -16,6 +16,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
 );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://github-app-mc2n.onrender.com",
+        target: "https://github-app-api.onrender.com",
       },
     },
   },
