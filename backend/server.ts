34256b2460ba5217e205767917f0a37888f5ad27
kathir <kathir@centiizen.com>
2024-03-13 15:22:30 +0530

    changes in cors

--- backend/server.ts
+++ backend/server.ts
@@ -16,7 +16,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "https://github-app01.netlify.app",
+    credentials: true,
+  })
+);
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
