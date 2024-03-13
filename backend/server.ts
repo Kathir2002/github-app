e0328a5c5e3642daac4b01ab78a3bc80005189e7
kathir <kathir@centiizen.com>
2024-03-13 15:04:26 +0530

    changes in server.js file

--- backend/server.ts
+++ backend/server.ts
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
