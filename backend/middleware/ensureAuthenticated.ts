afd552975ab28e07741b4cb1a32017444930c25a
kathir <kathir@centiizen.com>
2024-03-13 15:01:37 +0530

    changes in headers

--- backend/middleware/ensureAuthenticated.ts
+++ backend/middleware/ensureAuthenticated.ts
@@ -2,5 +2,7 @@ export async function ensureAuthenticated(req: any, res: any, next: any) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect(process.env.CLIENT_BASE_URL + "/login");
+  res.res
+    .header("Access-Control-Allow-Origin", "*")
+    .redirect(process.env.CLIENT_BASE_URL + "/login");
 }
