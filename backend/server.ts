0f954490187884d2220d790e0b1fd0df51c226fb
kathir <kathir@centiizen.com>
2024-04-16 15:41:09 +0530

    bug fixes

--- backend/server.ts
+++ backend/server.ts
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "https://githup-app.netlify.app/",
+    origin: "https://githup-app.netlify.app",
     credentials: true,
   })
 );
