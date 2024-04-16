4cdcaa8fd6a3320470beb9b638dffee9fc8a4f2b
kathir <kathir@centiizen.com>
2024-04-16 15:36:29 +0530

    added orgin

--- backend/server.ts
+++ backend/server.ts
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "https://github-app.cyclic.app",
+    origin: "https://githup-app.netlify.app/",
     credentials: true,
   })
 );
